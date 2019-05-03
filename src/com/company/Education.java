79866cc12a879fc525899fcdb9251dc7432457fa
Yifty <yifty08@icloud.com>
2019-05-03 15:07:11 -0400

    just started

--- src/com/company/Education.java
+++ src/com/company/Education.java
@@ -1,4 +1,50 @@
 package com.company;
 
-public class Education {
+public class Education extends Info {
+
+    private String degree, major, uniName;
+    private int gradYear;
+
+    public Education() {
+
+    }
+
+    public Education(String degree, String major, String uniName, int gradYear) {
+        this.degree = degree;
+        this.major = major;
+        this.uniName = uniName;
+        this.gradYear = gradYear;
+    }
+
+    public String getDegree() {
+        return degree;
+    }
+
+    public void setDegree(String degree) {
+        this.degree = degree;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public String getUniName() {
+        return uniName;
+    }
+
+    public void setUniName(String uniName) {
+        this.uniName = uniName;
+    }
+
+    public int getGradYear() {
+        return gradYear;
+    }
+
+    public void setGradYear(int gradYear) {
+        this.gradYear = gradYear;
+    }
 }
---
+++ src/com/company/Resume.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Resume {
+
+    Info person1 = new Info("John Doe", "jdoe@email.com", {"sfsdf", "sdfsf", "sfsf", 2019});
+
+}
